a3564c480003214475765555642f77bf9eb26948
abeshahsan <abeshahsan2002@gmail.com>
2024-01-24 23:43:15 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./App.css"
+
+function App() {
+    return (
+        <>
+        <div className="main-container">
+            
+            <div className="container ch-chessboard">
+                <div className="row justify-content-center">
+                    <div className="col-sm">
+                        One of three columns
+                    </div>
+                    <div className="col-sm">
+                        One of three columns
+                    </div>
+                    <div className="col-sm">
+                        One of three columns
+                    </div>
+                </div>
+            </div>
+        </div>
+        </>
+    );
+}
+
+export default App;
