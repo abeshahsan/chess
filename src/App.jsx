0c5d7c3a88e5ddb17f9ac714e6e891d84874d698
abeshahsan <abeshahsan2002@gmail.com>
2024-03-16 14:58:18 +0600

    king check and checkmate done finally

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./App.css"
 
-import Grid from "./GridComponents/grid";
 import { GridMatrixContextProvider, PiecesInfoContextProvider, blackPieces, whitePieces } from "./contexts/chessboard-context";
 
 const App = () => {
-    let gridMatrix;
     return (
         <>
             <div className="main-container">
                 <GridMatrixContextProvider>
-                    <PiecesInfoContextProvider>
-                    </PiecesInfoContextProvider>
+                    <PiecesInfoContextProvider/>
                 </GridMatrixContextProvider>
             </div>
         </>
